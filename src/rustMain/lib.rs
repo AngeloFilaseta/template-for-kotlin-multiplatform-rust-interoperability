ecd6379cb6cebb650414d33038a8b6e6ee62541b
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-08-31 11:34:42 +0200

    test(rust): add plus test in sources

--- src/rustMain/lib.rs
+++ src/rustMain/lib.rs
@@ -6,3 +6,19 @@ pub mod plus {
         a + b
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use plus::plus;
+
+    #[test]
+    fn plus_test() {
+        assert_eq!(0, plus(0, 0));
+        assert_eq!(0, plus(-1, 1));
+        assert_eq!(0, plus(1, -1));
+        assert_eq!(4, plus(2, 2));
+        assert_eq!(5, plus(3, 2));
+        assert_eq!(5, plus(2, 3));
+    }
+
+}
